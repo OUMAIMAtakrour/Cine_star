dd3cfd310aa756b80280c04be79da3ff9561c8b8
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-09-27 00:48:36 +0100

    Feat:film crud is done but still need to fix the object id

--- src/routes/filmRoutes.js
+++ src/routes/filmRoutes.js
@@ -2,7 +2,6 @@
 
 
 
-
 const express = require('express');
 const filmController = require('../controllers/filmController');
 const authMiddleware = require('../middleware/authMiddleware');
@@ -11,5 +10,9 @@ const router = express.Router();
 
 
 router.post('/create', authMiddleware, roleMiddleware(['admin']), filmController.store.bind(filmController));
+router.get('/', filmController.index.bind(filmController));
+router.get('/:id', filmController.show.bind(filmController));
+router.put('/:id', authMiddleware, roleMiddleware(['admin']), filmController.update.bind(filmController));
+router.delete('/:id', authMiddleware, roleMiddleware(['admin']), filmController.destroy.bind(filmController));
 
 module.exports = router;
